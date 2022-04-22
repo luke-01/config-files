453796478084477939ab412538b828feeefe814d
Lucas Galindo <lanternmaze@gmail.com>
2022-04-22 17:20:19 -0400

    changed terminal settings for neovim

--- nvim/.config/nvim/lua/luke/keymaps.lua
+++ nvim/.config/nvim/lua/luke/keymaps.lua
@@ -31,6 +31,10 @@ set_keymap('n', '<', '<gv', noremap)
 
 -- escape from terminal mode
 set_keymap('t', '<Esc>', [[<C-\><C-n>]], noremap)
+set_keymap('t', '<C-l>', [[<C-\><C-n><C-w>l]], noremap)
+set_keymap('t', '<C-k>', [[<C-\><C-n><C-w>k]], noremap)
+set_keymap('t', '<C-j>', [[<C-\><C-n><C-w>j]], noremap)
+set_keymap('t', '<C-h>', [[<C-\><C-n><C-w>h]], noremap)
 
 -- launch fuzzy finder
 set_keymap('n', '<leader>f', '<cmd>Telescope find_files<CR>', noremap_silent)
